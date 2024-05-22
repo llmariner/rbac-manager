72bf0055635cd7c9f07b85da337d7cb7dd55217f
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-21 23:33:58 -0700

    Add KuberenetesNamespace to UserInfo

--- pkg/auth/userinfo.go
+++ pkg/auth/userinfo.go
@@ -10,9 +10,10 @@ type userInfoKey struct{}
 
 // UserInfo manages the user info.
 type UserInfo struct {
-	UserID         string
-	OrganizationID string
-	ProjectID      string
+	UserID               string
+	OrganizationID       string
+	ProjectID            string
+	KuberenetesNamespace string
 }
 
 func appendUserInfoToContext(ctx context.Context, info UserInfo) context.Context {
@@ -20,15 +21,16 @@ func appendUserInfoToContext(ctx context.Context, info UserInfo) context.Context
 }
 
 // ExtractUserInfoFromContext extracts the user info from the context.
-func ExtractUserInfoFromContext(ctx context.Context) (*UserInfo, bool) {
+func ExtractUserInfoFromContexta(ctx context.Context) (*UserInfo, bool) {
 	info, ok := ctx.Value(userInfoKey{}).(*UserInfo)
 	return info, ok
 }
 
 func newUserInfoFromAuthorizeResponse(resp *v1.AuthorizeResponse) UserInfo {
 	return UserInfo{
-		UserID:         resp.User.Id,
-		OrganizationID: resp.Organization.Id,
-		ProjectID:      resp.Project.Id,
+		UserID:               resp.User.Id,
+		OrganizationID:       resp.Organization.Id,
+		ProjectID:            resp.Project.Id,
+		KuberenetesNamespace: resp.Project.KubernetesNamespace,
 	}
 }
