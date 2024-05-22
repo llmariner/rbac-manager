9ebbea16c69f1d801346276eebb89f68b361727d
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-21 23:46:21 -0700

    Fix a typo: KuberenetesNamespace -> KubernetesNamespace

--- pkg/auth/userinfo.go
+++ pkg/auth/userinfo.go
@@ -10,10 +10,10 @@ type userInfoKey struct{}
 
 // UserInfo manages the user info.
 type UserInfo struct {
-	UserID               string
-	OrganizationID       string
-	ProjectID            string
-	KuberenetesNamespace string
+	UserID              string
+	OrganizationID      string
+	ProjectID           string
+	KubernetesNamespace string
 }
 
 func appendUserInfoToContext(ctx context.Context, info UserInfo) context.Context {
@@ -28,9 +28,9 @@ func ExtractUserInfoFromContext(ctx context.Context) (*UserInfo, bool) {
 
 func newUserInfoFromAuthorizeResponse(resp *v1.AuthorizeResponse) UserInfo {
 	return UserInfo{
-		UserID:               resp.User.Id,
-		OrganizationID:       resp.Organization.Id,
-		ProjectID:            resp.Project.Id,
-		KuberenetesNamespace: resp.Project.KubernetesNamespace,
+		UserID:              resp.User.Id,
+		OrganizationID:      resp.Organization.Id,
+		ProjectID:           resp.Project.Id,
+		KubernetesNamespace: resp.Project.KubernetesNamespace,
 	}
 }
