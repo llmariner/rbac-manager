5a38ea248eec64266273cafcc67465d6ff7983ec
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-24 14:41:14 -0700

    Add CarryMetadataFromHTTPHeader (#54)
    This is used to make an RPC call from an HTTP handler (e.g.,
    call Model Manager from the completion HTTP handler).

--- pkg/auth/metadata.go
+++ pkg/auth/metadata.go
@@ -2,13 +2,14 @@ package auth
 
 import (
 	"context"
+	"net/http"
 	"strings"
 
 	"google.golang.org/grpc/metadata"
 )
 
 // CarryMetadata extracts relevant metadata from the incoming context
-// and append that to the outgoing context.
+// and appends that to the outgoing context.
 func CarryMetadata(ctx context.Context) context.Context {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
@@ -28,6 +29,24 @@ func CarryMetadata(ctx context.Context) context.Context {
 		}
 
 		ctx = metadata.AppendToOutgoingContext(ctx, header, v[0])
+	}
+	return ctx
+}
+
+// CarryMetadataFromHTTPHeader extracts relevant metadata from HTTP headers
+// and appends that to the outgoing context.
+func CarryMetadataFromHTTPHeader(ctx context.Context, header http.Header) context.Context {
+	keys := []string{
+		authHeader,
+		orgHeader,
+		projectHeader,
+	}
+	for _, k := range keys {
+		v, ok := header[k]
+		if !ok {
+			continue
+		}
+		ctx = metadata.AppendToOutgoingContext(ctx, k, v[0])
 	}
 	return ctx
 }
