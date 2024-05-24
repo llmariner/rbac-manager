f7a0c02ee8983aaf1b9e1c0ed2b8d28c4cd7af33
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-24 14:38:12 -0700

    Add CarryMetadataFromHTTPHeader
    This is used to make an RPC call from an HTTP handler (e.g.,
    call Model Manager from the completion HTTP handler).

--- pkg/auth/metadata_test.go
+++ pkg/auth/metadata_test.go
@@ -25,3 +25,18 @@ func TestCarryMetadata(t *testing.T) {
 	assert.ElementsMatch(t, []string{"o0"}, got.Get(orgHeader))
 	assert.ElementsMatch(t, []string{"p0"}, got.Get(projectHeader))
 }
+
+func TestCarryMetadataFromHTTPHeader(t *testing.T) {
+	header := map[string][]string{
+		authHeader:    {"a0"},
+		orgHeader:     {"o0"},
+		projectHeader: {"p0"},
+	}
+
+	ctx := CarryMetadataFromHTTPHeader(context.Background(), header)
+	got, ok := metadata.FromOutgoingContext(ctx)
+	assert.True(t, ok)
+	assert.ElementsMatch(t, []string{"a0"}, got.Get(authHeader))
+	assert.ElementsMatch(t, []string{"o0"}, got.Get(orgHeader))
+	assert.ElementsMatch(t, []string{"p0"}, got.Get(projectHeader))
+}
