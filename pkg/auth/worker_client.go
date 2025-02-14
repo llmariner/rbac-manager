029eb4f490231fb43973372f16e5d5823ec5b879
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-02-14 14:50:22 -0800

    feat(pkg/auth): add ValidateClusterRegistrationKey
    It is often difficult to debug when a bogus cluster registation key is set.
    This function is helpful to catch such errors more clearly.

--- pkg/auth/worker_client.go
+++ pkg/auth/worker_client.go
@@ -5,24 +5,37 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"google.golang.org/grpc/metadata"
 )
 
 const (
-	envVarname = "LLMO_CLUSTER_REGISTRATION_KEY"
+	envVarName = "LLMO_CLUSTER_REGISTRATION_KEY"
 )
 
 // AppendWorkerAuthorization appends the authorization to the context for a request
 // from a worker cluster.
 func AppendWorkerAuthorization(ctx context.Context) context.Context {
-	key := os.Getenv(envVarname)
+	key := os.Getenv(envVarName)
 	auth := fmt.Sprintf("Bearer %s", key)
 	return metadata.AppendToOutgoingContext(ctx, "Authorization", auth)
 }
 
 // AppendWorkerAuthorizationToHeader appends the authorization to the HTTP header.
 func AppendWorkerAuthorizationToHeader(req *http.Request) {
-	key := os.Getenv(envVarname)
+	key := os.Getenv(envVarName)
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", key))
+}
+
+// ValidateClusterRegistrationKey validates the cluster registration key.
+func ValidateClusterRegistrationKey() error {
+	key := os.Getenv(envVarName)
+	if key == "" {
+		return fmt.Errorf("environment variable %s is not set", envVarName)
+	}
+	if !strings.HasPrefix(key, "clusterkey-") {
+		return fmt.Errorf("invalid cluster registration key")
+	}
+	return nil
 }
