a149ed2b2bed688055d38430adc8636c56866b72
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-02-14 16:20:27 -0800

    feat(pkg/auth): add ValidateClusterRegistrationKey (#147)
    It is often difficult to debug when a bogus cluster registation key is set.
    This function is helpful to catch such errors more clearly.

---
+++ pkg/auth/worker_client_test.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateClusterRegistrationKey(t *testing.T) {
+	tcs := []struct {
+		key     string
+		isError bool
+	}{
+		{
+			key:     "clusterkey-1234567890",
+			isError: false,
+		},
+		{
+			key:     "bogus",
+			isError: true,
+		},
+		{
+			key:     "",
+			isError: true,
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.key, func(t *testing.T) {
+			err := os.Setenv(envVarName, tc.key)
+			assert.NoError(t, err)
+			err = ValidateClusterRegistrationKey()
+			if tc.isError {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+		})
+	}
+}
