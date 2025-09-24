2bb68ccf342319e9b1de14004e01059502ae9437
Kenji Kaneda <kenji@kanedas-Mac-mini.local>
2025-09-24 14:46:47 -0700

    chore: register Reflection

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -11,6 +11,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
 )
 
 type cacheGetter interface {
@@ -62,6 +63,7 @@ type Server struct {
 func (s *Server) Run(ctx context.Context, port int) error {
 	serv := grpc.NewServer()
 	v1.RegisterRbacInternalServiceServer(serv, s)
+	reflection.Register(serv)
 
 	healthCheck := health.NewServer()
 	healthCheck.SetServingStatus("", grpc_health_v1.HealthCheckResponse_SERVING)
